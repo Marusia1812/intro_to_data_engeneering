b88826ead4ff4447e4b4eb0c8493248488ae3e93
Marusia1812 <118619037+Marusia1812@users.noreply.github.com>
2023-03-24 20:08:13 +0300

    Update care_providers.py

--- hw1/care_providers.py
+++ hw1/care_providers.py
@@ -7,19 +7,29 @@ from rdflib.namespace import QB, RDF, XSD
 NS = Namespace("https://mariiacinkova.github.io/ontology#")
 NSR = Namespace("https://mariiacinkova.github.io/resources/")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
-hashmap = {}
+SDMX_DIMENSION = Namespace("http://purl.org/linked-data/sdmx/2009/dimension#")
+SDMX_MEASURE = Namespace("http://purl.org/linked-data/sdmx/2009/measure#")
+
 absolute_path = os.path.dirname(__file__)
 
-
 def main():
-    data_as_csv = load_csv_file_as_object(absolute_path + "/care_providers.csv")
-    for data in data_as_csv:
-        data['measure'] = hashmap[(data['Obec'], data['DruhPece'])]
-    data_cube = as_data_cube(data_as_csv)
-    f = open(absolute_path + "/care_providers.ttl", "w")
+    hashmap = county_codelist_create()
+    data_as_csv = load_csv_file_as_object(absolute_path + "/population.csv")
+    data_cube = as_data_cube(data_as_csv, hashmap)
+    f = open(absolute_path + "/population.ttl", "w")
     f.write(data_cube.serialize(format="ttl"))
     f.close()
-    
+
+
+def county_codelist_create():
+    result = {}
+    with open(absolute_path + "/county_codelist.csv", "r") as stream:
+        reader = csv.reader(stream)
+        next(reader)
+        for line in reader:
+            result[line[8]] = line[4]
+    return result
+
 
 def load_csv_file_as_object(file_path: str):
     result = []
@@ -27,22 +37,18 @@ def load_csv_file_as_object(file_path: str):
         reader = csv.reader(stream)
         header = next(reader)  # Skip header
         for line in reader:
-            if (line[5], line[29]) in hashmap.keys():
-                hashmap[(line[5], line[29])] += 1
-            else:
-                hashmap[(line[5], line[29])] = 1
-
-            result.append({key: value for key, value in zip(header, line)})
+            if line[2] == 'DEM0004' and line[5] == '101':
+                result.append({key: value for key, value in zip(header, line)})
     return result
 
 
-def as_data_cube(data):
+def as_data_cube(data, hashmap):
     result = Graph()
     dimensions = create_dimensions(result)
     measures = create_measure(result)
     structure = create_structure(result, dimensions, measures)
     dataset = create_dataset(result, structure)
-    create_observations(result, dataset, data)
+    create_observations(result, dataset, data, hashmap)
     return result
 
 
@@ -54,6 +60,8 @@ def create_dimensions(collector: Graph):
     collector.add((county, RDFS.label, Literal("Okres", lang="cs")))
     collector.add((county, RDFS.label, Literal("County", lang="en")))
     collector.add((county, RDFS.range, XSD.string))
+    collector.add((county, RDFS.subPropertyOf, SDMX_DIMENSION.refArea))
+    collector.add((county, QB.concept, SDMX_DIMENSION.refArea))
 
     region = NS.region
     collector.add((region, RDF.type, RDFS.Property))
@@ -61,15 +69,10 @@ def create_dimensions(collector: Graph):
     collector.add((region, RDFS.label, Literal("Kraj", lang="cs")))
     collector.add((region, RDFS.label, Literal("Region", lang="en")))
     collector.add((region, RDFS.range, XSD.string))
+    collector.add((region, RDFS.subPropertyOf, SDMX_DIMENSION.refArea))
+    collector.add((region, QB.concept, SDMX_DIMENSION.refArea))
 
-    field_of_care = NS.field_of_care
-    collector.add((field_of_care, RDF.type, RDFS.Property))
-    collector.add((field_of_care, RDF.type, QB.DimensionProperty))
-    collector.add((field_of_care, RDFS.label, Literal("OborPece", lang="cs")))
-    collector.add((field_of_care, RDFS.label, Literal("FieldOfCare", lang="en")))
-    collector.add((field_of_care, RDFS.range, XSD.string))
-
-    return [county, region, field_of_care]
+    return [county, region]
 
 
 def create_measure(collector: Graph):
@@ -77,9 +80,11 @@ def create_measure(collector: Graph):
     measure = NS.measure
     collector.add((measure, RDF.type, RDFS.Property))
     collector.add((measure, RDF.type, QB.MeasureProperty))
-    collector.add((measure, RDFS.label, Literal("Pocet", lang="cs")))
-    collector.add((measure, RDFS.label, Literal("Measure", lang="en")))
+    collector.add((measure, RDFS.label, Literal("StredniHodnota", lang="cs")))
+    collector.add((measure, RDFS.label, Literal("Mean", lang="en")))
     collector.add((measure, RDFS.range, XSD.integer))
+    collector.add((measure, RDFS.subPropertyOf, SDMX_MEASURE.obsValue))
+    collector.add((measure, QB.concept, SDMX_MEASURE.obsValue))
 
     return [measure]
 
@@ -107,26 +112,26 @@ def create_dataset(collector: Graph, structure):
     dataset = NSR.dataCubeInstance
     collector.add((dataset, RDF.type, QB.DataSet))
     collector.add((dataset, RDFS.label, Literal(
-        "Care Providers", lang="en")))
+        "Population 2021", lang="en")))
     collector.add((dataset, QB.structure, structure))
 
     return dataset
 
 
-def create_observations(collector: Graph, dataset, data):
+def create_observations(collector: Graph, dataset, data, hashmap):
     for index, row in enumerate(data):
         resource = NSR["observation-" + str(index).zfill(3)]
-        create_observation(collector, dataset, resource, row)
+        create_observation(collector, dataset, resource, row, hashmap)
 
 
-def create_observation(collector: Graph, dataset, resource, data):
+def create_observation(collector: Graph, dataset, resource, data, hashmap):
     collector.add((resource, RDF.type, QB.Observation))
     collector.add((resource, QB.dataSet, dataset))
-    collector.add((resource, NS.county, Literal(data["Okres"], lang="cs")))
-    collector.add((resource, NS.region, Literal(data["Kraj"], lang="cs")))
-    collector.add((resource, NS.field_of_care, Literal(data["NazevZarizeni"], lang="cs")))
+    if data['vuzemi_cis'] == "101":
+        collector.add((resource, NS.county, Literal(data["vuzemi_txt"], lang="cs")))
+        collector.add((resource, NS.region, Literal(hashmap[data["vuzemi_kod"]], lang="cs")))
     collector.add((resource, NS.measure, Literal(
-        data["measure"], datatype=XSD.integer)))
+        data["hodnota"], datatype=XSD.integer)))
 
 
 if __name__ == "__main__":
